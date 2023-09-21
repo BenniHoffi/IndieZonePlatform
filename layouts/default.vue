1b176e881f6263530b4c5eefb524a54406e026f5
ss58qyku <ss58qyku@studserv.uni-leipzig.de>
2023-09-21 12:13:51 +0200

    Shop-Banner quasi done

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,12 +1,11 @@
 <template>
-  <div class="font-montserrat flex flex-col h-screen w-full">
-    <MainHeader />
-    <main class="grow">
-      <slot />
-    </main>
-    <MainFooter />
-  </div>
+    <div class="font-montserrat flex flex-col items-center h-screen w-full">
+        <MainHeader />
+        <main class="grow lg:max-w-7xl">
+            <slot />
+        </main>
+        <MainFooter />
+    </div>
 </template>
 
-<script lang="ts" setup>
-</script>
+<script lang="ts" setup></script>
--- pages/shop-[id]/components/ShopBannerSection.vue
+++ pages/shop-[id]/components/ShopBannerSection.vue
@@ -1,62 +1,92 @@
 <template>
-  <div class="w-full max-w-lg">
-        <div class="relative text-white">
-          <!-- TODO: URL richtig machen -->
-            <img class="w-full" src="~/assets/dummyImages/shoppy.png" />
-            <button class="absolute left-3 top-3" @click="router.back()">
-                <span class="material-symbols-outlined">arrow_back</span>
-            </button>
-            <button class="absolute right-3 top-3" @click="">
-                <span class="material-symbols-outlined">favorite</span>
-            </button>
-        </div>
-        <div class="p-4">
-            <div class="flex justify-between">
-                <h1 class="title-large">{{ shop.name }}</h1>
-                <div class="text-secondary">
-                    <span v-if="shop.rating > 0" class="material-symbols-outlined">star</span>
-                    <span v-if="shop.rating > 1" class="material-symbols-outlined">star</span>
-                    <span v-if="shop.rating > 2" class="material-symbols-outlined">star</span>
-                    <span v-if="shop.rating > 3" class="material-symbols-outlined">star</span>
-                    <span v-if="shop.rating > 4" class="material-symbols-outlined">star</span>
+    <section class="flex flex-col w-full max-w-6xl lg:flex-row lg:items-end">
+        <div class="flex lg:w-2/3">
+            <div class="hidden w-1/5 items-start lg:flex">
+                <button class="hidden lg:block" @click="router.back()">
+                    <span class="material-symbols-outlined">arrow_back</span>
+                </button>
+                <div class="px-4 space-y-2">
+                    <img class="w-full h-auto" src="~/assets/dummyImages/shoppy.png" />
+                    <img class="w-full h-auto" src="~/assets/dummyImages/shoppy.png" />
+                    <img class="w-full h-auto" src="~/assets/dummyImages/shoppy.png" />
+                    <img class="w-full h-auto" src="~/assets/dummyImages/shoppy.png" />
                 </div>
             </div>
+            <div class="relative text-white lg:w-4/5">
+                <!-- TODO: URL richtig machen -->
+                <img class="w-full" src="~/assets/dummyImages/shoppy.png" />
+                <button class="absolute left-3 top-3 lg:hidden" @click="router.back()">
+                    <span class="material-symbols-outlined">arrow_back</span>
+                </button>
+                <button class="absolute right-3 top-3 lg:hidden" @click="">
+                    <span class="material-symbols-outlined">favorite</span>
+                </button>
+            </div>
+        </div>
+        <div class="px-4 py-4 space-y-4 lg:w-1/3 lg:py-0">
+            <div class="space-y-1">
+                <div class="flex justify-between">
+                    <h1 class="title-large">{{ shop.name }}</h1>
+                    <div class="text-secondary">
+                        <span v-if="shop.rating > 0" class="material-symbols-outlined">star</span>
+                        <span v-if="shop.rating > 1" class="material-symbols-outlined">star</span>
+                        <span v-if="shop.rating > 2" class="material-symbols-outlined">star</span>
+                        <span v-if="shop.rating > 3" class="material-symbols-outlined">star</span>
+                        <span v-if="shop.rating > 4" class="material-symbols-outlined">star</span>
+                    </div>
+                </div>
 
-            <div class="text-label-medium font-bold">
-                {{ shop.intro }}
+                <div class="text-label-medium font-bold">
+                    {{ shop.intro }}
+                </div>
+                <div class="flex text-label-small items-center gap-1">
+                    <span class="material-symbols-outlined"> schedule </span>
+                    Heute geöffnet bis 18:00 Uhr
+                    <span
+                        class="material-symbols-outlined cursor-pointer transition-transform"
+                        :class="openingHoursOpen ? 'rotate-90' : ''"
+                        @click="openingHoursOpen = !openingHoursOpen">
+                        arrow_right
+                    </span>
+                </div>
             </div>
             <div>
-              TODO Öffnungszeiten
-            </div>
-            <div class ="text-label-small">
-              {{ shop.streetAdress }}
-            </div>
-            <div class ="text-label-small">
-              {{ shop.zip }} {{ shop.city }} (--km)
-            </div>
-            <div class="flex justify-between">
-              <div>
-                <div class ="text-label-small">
-                  {{ shop.phone }}
+                <div class="text-label-small">
+                    {{ shop.streetAdress }}
                 </div>
-                <div class ="text-label-small">
-                  {{ shop.email }}
+                <div class="text-label-small">{{ shop.zip }} {{ shop.city }} (--km)</div>
+                <div class="flex justify-between items-center">
+                    <div>
+                        <div class="text-label-small">
+                            {{ shop.phone }}
+                        </div>
+                        <div class="text-label-small">
+                            {{ shop.email }}
+                        </div>
+                    </div>
+                    <Button size="small" @click="">
+                        <template #icon>
+                            <span class="material-symbols-outlined">chat_bubble</span>
+                        </template>
+                    </Button>
                 </div>
-              </div>
-              <Button class="bg-bluegray-600 hover:bg-primary-hover border-bluegray-700" size="small" @click="">
-                <span class="material-symbols-outlined">chat</span>
-              </Button>
             </div>
-            <div class="flex justify-around">
-              <Button label="Auf meine Route" size="small"></Button>
-              <Button label="Auf karte anzeigen" size="small" outlined></Button>
+            <div>
+                <div class="flex justify-between gap-2">
+                    <Button label="Auf meine Route" size="small" />
+                    <Button label="Auf Karte anzeigen" size="small" outlined />
+                </div>
             </div>
         </div>
-    </div>
+    </section>
 </template>
 
 <script lang="ts" setup>
-  const props = defineProps<{
-    shop: any,
-  }>();
+    const props = defineProps<{
+        shop: any
+    }>()
+
+    const router = useRouter()
+
+    const openingHoursOpen = ref(false)
 </script>
